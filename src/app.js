19bd672d745bad79005859582d965eb1e07269a5
Chu A. <chu.amk22@gmail.com>
2026-02-12 08:10:16 +0100

    feat(swagger): add Swagger documentation and JSON endpoint for API

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ import createError from "http-errors";
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./config/swagger.js";
 
 import authRouter from "../src/modules/auth/auth.routes.js";
 import userRouter from "../src/modules/users/user.routes.js";
+import providerRouter from "../src/modules/providers/provider.routes.js";
+import timeSlotRouter from "../src/modules/timeSlots/timeSlot.routes.js";
+import appointmentRouter from "../src/modules/appointments/appointment.routes.js";
 
 const app = express();
 
@@ -14,8 +19,24 @@ app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Swagger documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  explorer: true,
+  customCss: '.swagger-ui .topbar { display: none }',
+  customSiteTitle: "Appointment Booking API Docs"
+}));
+
+// Swagger JSON endpoint
+app.get("/api-docs.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
+app.use("/providers", providerRouter);
+app.use("/time-slots", timeSlotRouter);
+app.use("/appointments", appointmentRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
